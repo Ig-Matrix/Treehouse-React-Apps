94d00a3f910bcabe4f80234a08e20b07927e544f
Ig-Matrix <omomaighawosa440@gmail.com>
2023-09-27 04:07:55 +0100

    added preserving user state with cookies to react-auth app

--- react-auth/client/src/context/UserContext.js
+++ react-auth/client/src/context/UserContext.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { createContext } from "react";
+import Cookies from "js-cookie";
 
 const UserContext = createContext(null);
 
 export const UserProvider = (props) => {
-   const [authUser, setAuthUser] = useState(null);
+   const cookie = Cookies.get("authenticatedUser");
+   const [authUser, setAuthUser] = useState(cookie ? JSON.parse(cookie) : null); //checks if cookie exists across page refreshes
 
    const signIn = async (credentials) => {
       const encodedCredentials = btoa(
@@ -23,6 +25,7 @@ export const UserProvider = (props) => {
       if (response.status === 200) {
          const user = await response.json();
          setAuthUser(user);
+         Cookies.set("authenticatedUser", JSON.stringify(user), { expires: 1 });
          return user;
       } else if (response.status === 400) {
          return null;
@@ -31,10 +34,13 @@ export const UserProvider = (props) => {
       }
    };
 
-   const signOut = () => setAuthUser(null);
+   const signOut = () => {
+      setAuthUser(null)
+      Cookies.remove('authenticatedUser') //removes the saved cookie when the user signs out
+   };
 
    return (
-      <UserContext.Provider value={{ authUser, actions: { signIn, signOut}  }}>
+      <UserContext.Provider value={{ authUser, actions: { signIn, signOut } }}>
          {props.children}
       </UserContext.Provider>
    );
