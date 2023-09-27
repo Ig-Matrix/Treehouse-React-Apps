f2e4aae239cfef9aa86d1767b62d556dc5115a02
Ig-Matrix <omomaighawosa440@gmail.com>
2023-09-27 04:28:49 +0100

    added preserving user theme states with cookies to react-auth app

--- react-auth/client/src/context/ThemeContext.js
+++ react-auth/client/src/context/ThemeContext.js
@@ -1,11 +1,18 @@
+import Cookies from "js-cookie";
 import { createContext, useState, useEffect } from "react";
 
 const ThemeContext = createContext(null);
 
 export const ThemeProvider = (props) => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  const [accentColor, setAccentColor] = useState('#63537d');
-  const [fontPercentage, setFontPercentage] = useState(100);
+  const cookie = Cookies.get('defaultTheme')
+  const defaultTheme = cookie ? JSON.parse(cookie) : {
+    isDarkMode:false,
+    accentColor:'#63537d',
+    fontPercentage:100
+  }
+  const [isDarkMode, setIsDarkMode] = useState(defaultTheme.isDarkMode);
+  const [accentColor, setAccentColor] = useState(defaultTheme.accentColor);
+  const [fontPercentage, setFontPercentage] = useState(defaultTheme.fontPercentage);
 
   useEffect(() => {
     if (isDarkMode) {
@@ -14,7 +21,15 @@ export const ThemeProvider = (props) => {
       document.body.classList.remove('dark');
     }
     document.body.style.fontSize = `${fontPercentage}%`;
-  }, [isDarkMode, fontPercentage]);
+
+    const theme = {
+      isDarkMode,
+      fontPercentage,
+      accentColor
+    }
+
+    Cookies.set('defaultTheme', JSON.stringify(theme)) //cookies.set not only creates a new cookie, but it updates the cokies value if you provide  a name of a cookie that already exists
+  }, [isDarkMode, fontPercentage, accentColor]);
 
   const toggleDarkMode = () => {
     setIsDarkMode(currentMode => !currentMode);
